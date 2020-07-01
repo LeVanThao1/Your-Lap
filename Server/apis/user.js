ee141111e7fe9a25a913972b6839414539f3e082
Le Van Thao <vanthaodhdt@gmail.com>
2020-07-01 12:54:25 +0700

    fix

--- Server/apis/user.js
+++ Server/apis/user.js
@@ -4,14 +4,14 @@ const userValidation = require('../validations/user');
 const { validate }= require('express-validation');
 
 exports.load = (app) => {
-    app.post('/api/v1/users',validate(userValidation.creatUser()) ,controllerUser.createUser);
-    app.post('/api/v1/login', validate(userValidation.login()) ,controllerUser.login);
-    app.delete('/api/v1/users/:id', validate(userValidation.deleteUser()) ,controllerUser.deleteUser);
-    app.get('/api/v1/users/:id',  validate(userValidation.getUser()) ,controllerUser.getUser);
+    app.post('/api/v1/users' ,controllerUser.createUser);
+    app.post('/api/v1/login',controllerUser.login);
+    app.delete('/api/v1/users/:id' ,controllerUser.deleteUser);
+    app.get('/api/v1/users/:id',controllerUser.getUser);
     app.get( '/api/v1/users', controllerUser.getAllUser);
-    app.put('/api/v1/users/:id',  validate(userValidation.updateUser()) ,controllerUser.updateUser);
-    app.post('/api/v1/users/forget-password', validate(userValidation.forgetPassword()) ,controllerUser.forgetPassword);
-    app.post('/api/v1/users/reset-password', validate(userValidation.resetPassword()) ,controllerUser.resetPassword);
-    app.post('/api/v1/loginFB', validate(userValidation.loginFB()) ,controllerUser.loginFB);
+    app.put('/api/v1/users/:id' ,controllerUser.updateUser);
+    app.post('/api/v1/users/forget-password' ,controllerUser.forgetPassword);
+    app.post('/api/v1/users/reset-password',controllerUser.resetPassword);
+    app.post('/api/v1/loginFB',controllerUser.loginFB);
     // app.get('/api/v1/auth', validate(userValidation.creatUser) ,controllerUser.geUserWithToken)
 }
